9e01531d6f2424f7063d81bf5782e05577414f69
ksavav <89656360+ksavav@users.noreply.github.com>
2021-08-31 17:29:54 +0200

    Delete Minesweeper directory

--- Minesweeper/gameplay.py
+++
@@ -1,9 +0,0 @@
-from gameboard import GameBoard
-
-class GamePlay(GameBoard):
-    def __init__(self):
-        super().__init__()
-
-    def player_move(pos_x, pos_y): #pos_x is th number of the column, pos_y is th number of the row
-        
-        pass
